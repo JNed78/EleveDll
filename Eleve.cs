6ef649e73c0063428191458eb0f3f6ac3e970c05
JNed78 <jng0549@gmail.com>
2022-01-31 12:07:37 +0100

    Ajoutez des fichiers projet.

---
+++ Eleve.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EleveDll
+{
+    public class Eleve
+    {
+        private string nom;
+        private float note_oral, note_ecrit;
+
+        public string Nom { get => nom; set => nom = value; }
+        public float Oral { get => note_oral; set => note_oral = value; }
+        public float Ecrit { get => note_ecrit; set => note_ecrit = value; }
+
+        public Eleve(string nom, float note_oral, float note_ecrit)
+        {
+            this.Nom = nom;
+            this.Oral = note_oral;
+            this.Ecrit = note_ecrit;
+        }
+        private float moyenne()
+        {
+            return  (2 * this.Ecrit + this.Oral) / 3;
+        }
+        public  string afficher()
+        {
+            return this.nom + moyenne() + "La meilleur moyenne est " + meilleur() + "de" + Nom + "."; ;
+        }
+       
+        public override string ToString()
+        {
+            return nom + "." + Ecrit.ToString() + "." + Oral.ToString();
+        }
+        public static string meilleur()
+        {
+            if ()
+            {
+
+            }
+        }
+
+    }
+}
+
